1b743884fd1f4c6e5b99dd9b75ed4531b64441e3
Andreas Hemmingsson <andhem@gmail.com>
2018-05-01 00:46:22 +0200

    removed hello world

--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,6 @@ import RealTimeDeparturecontroller from './realTimeDeparturecontroller'
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    RealTimeDeparturecontroller.helloWorld(req, res);
-});
-
 router.get("/next", (req, res) => {
     RealTimeDeparturecontroller.getNextDeparture(req, res);
 });
